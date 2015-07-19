f1be91b40440442c46e97648b09b72b5adc8a3ef
Kiril Seksenov <seksenov@outlook.com>
2015-07-18 19:05:27 -0700

    Update 2015-07-19T02:05:15.739Z

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-"use strict";
+'use strict';
 
 // Handle Cortana activation adding the event listener before DOM Content Loaded
 // parse out the command type and call the respective game APIs
 
-if (typeof Windows !== "undefined") {
-  console.log("Windows namespace is available");
+if (typeof Windows !== 'undefined') {
+  console.log('Windows namespace is available');
   // Subscribe to the Windows Activation Event
-  Windows.UI.WebUI.WebUIApplication.addEventListener("activated", function (args) {
+  Windows.UI.WebUI.WebUIApplication.addEventListener('activated', function (args) {
     var activation = Windows.ApplicationModel.Activation;
     // Check to see if the app was activated by a voice command
     if (args.kind === activation.ActivationKind.voiceCommand) {
@@ -15,20 +15,23 @@ if (typeof Windows !== "undefined") {
       var textSpoken = speechRecognitionResult.text;
       var command = speechRecognitionResult.rulePath[0];
 
-      console.log("The command is: " + command);
+      console.log('The command is: ' + command);
+      document.getElementById('command').innerHTML = command;
+      document.getElementById('speechReco').innerHTML = speechRecognitionResult;
+      document.getElementById('text').innerHTML = textSpoken;
 
       // Determine the command type {play} defined in vcd
-      if (command === "send") {
+      if (command === 'send') {
         // Determine the stream name specified
-        console.log("The speech reco result is: " + speechRecognitionResult);
-        console.log("The text spoken is: " + textSpoken);
+        console.log('The speech reco result is: ' + speechRecognitionResult);
+        console.log('The text spoken is: ' + textSpoken);
       }
       else { 
         // No valid command specified
-        console.log("No valid command specified");
+        console.log('No valid command specified');
       }
     }
   });
 } else {
-  console.log("Windows namespace is unavaiable");
+  console.log('Windows namespace is unavaiable');
 }
